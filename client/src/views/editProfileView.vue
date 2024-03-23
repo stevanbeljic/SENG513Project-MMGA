f59d81699c8d52aaa61209c1e35241bb22db848f
williamchan2 <87112587+williamchan2@users.noreply.github.com>
2024-03-23 17:30:51 -0600

    user profile for desktop implemented

---
+++ client/src/views/editProfileView.vue
@@ -0,0 +1,17 @@
+<script setup>
+    import "../assets/footer.css";
+import "../assets/header.css";
+import "../assets/home.css";
+import navbar from "../components/navbarView.vue";
+</script>
+<template>
+    <head>
+        <link href='https://fonts.googleapis.com/css?family=Kanit' rel='stylesheet'>
+    </head>
+    <header class = "header-section">
+        <navbar></navbar>
+    </header>
+    <div class="page-container">
+        Edit Profile
+    </div>
+</template>
