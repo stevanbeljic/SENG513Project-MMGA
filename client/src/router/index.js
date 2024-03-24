895b80840da00f128d05b773b63b73875139e4ba
Justin-Chua <justin.chua@ucalgary.ca>
2024-03-24 17:19:12 -0600

    add game desktop view complete - missing icons

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -8,6 +8,7 @@ import discussionView from '../views/discussionView.vue'
 import friendsView from '../views/friendsView.vue'
 import profileView from '../views/profileView.vue'
 import editProfileView from '../views/editProfileView.vue'
+import AddGameView from '../views/AddGameView.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -55,6 +56,11 @@ const router = createRouter({
       path: '/editProfile',
       name: 'editProfile',
       component: editProfileView
+    },
+    {
+      path: '/addGame',
+      name: 'addGame',
+      component: AddGameView
     }
   ]
 })
---
+++ client/src/views/AddGameView.vue
@@ -0,0 +1,50 @@
+<script setup>
+    import "../assets/footer.css";
+    import "../assets/header.css";
+    import "../assets/addGame.css";
+    import navbar from "../components/navbarView.vue";
+</script>
+<template>
+    <head>
+        <link href='https://fonts.googleapis.com/css?family=Kanit' rel='stylesheet'>
+        <link href="https://fonts.googleapis.com/css2?family=Major+Mono+Display&display=swap" rel="stylesheet">
+    </head>
+    <header class ="header-section">
+        <navbar></navbar>
+    </header>
+    <body>
+        <div class="ag-body-container">
+            <div class="ag-basic-grid">
+                <div class="ag-title-section">
+                    <form>
+                        <input id="ag-title" type="text" placeholder="enter title here...">
+                    </form>
+                </div>
+                <div class="ag-preview-section">
+                    <img id="ag-preview-image" src="../components/icons/mario.jpg">
+                    <div class="preview-text"><strong>PREVIEW</strong></div>
+                </div>
+                <div class="ag-button-section">
+                    <button id="ag-upload-button">Upload Media</button>
+                </div>
+                <div class="ag-description-section">
+                    <form>
+                        <textarea id="ag-description" placeholder="Description"></textarea>
+                    </form>
+                </div>
+            </div>
+            <div class="ag-advanced-flex">
+                <div>
+                    <form class="ag-advanced-form">
+                        <input id="ag-link" type="text" placeholder="enter link here...">
+                        <input id="ag-price" type="text" placeholder="price"><br>
+                        <input id="ag-tags" type="text" placeholder="add tags...">
+                    </form>
+                </div>
+                <div>
+                    <button id="ag-publish-button"><strong>PUBLISH</strong></button>
+                </div>
+            </div>
+        </div>
+    </body>
+</template>
