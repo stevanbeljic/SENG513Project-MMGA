02e001bb8dd9e228850f3a80c6ff1bfc0095552f
NathanRomphf <101217605+NathanRomphf@users.noreply.github.com>
2024-03-24 22:15:56 -0600

    add functionality to loop through an array of game objects

--- client/src/views/catalogView.vue
+++ client/src/views/catalogView.vue
@@ -4,8 +4,33 @@ import "../assets/header.css";
 import "../assets/catalog.css";
 import navbar from "../components/navbarView.vue";
 import bottomNavbar from '@/components/bottomNavbarView.vue';
-import App from "@/App.vue";
 </script>
+
+<script>
+    let games = [
+        {id: 1 , title: 'Game1', developer: 'Developer1', Genre: 'Genre1', 
+            AppPrice: '$0.99', Playprice: '$0.99', img: '../components/icons/mario.jpg'},
+        {id: 2 ,title: 'Game2', developer: 'Developer2', Genre: 'Genre1', 
+            AppPrice: '$0.99', Playprice: '$0.99', img: '../components/icons/mario.jpg'},
+        {id: 3 ,title: 'Game3', developer: 'Developer3', Genre: 'Genre1', 
+            AppPrice: '$0.99', Playprice: '$0.99', img: '../components/icons/mario.jpg'},
+        {id: 4 ,title: 'Game4', developer: 'Developer4', Genre: 'Genre1', 
+            AppPrice: '$0.99', Playprice: '$0.99', img: '../components/icons/mario.jpg'},
+        {id: 5 ,title: 'Game5', developer: 'Developer2', Genre: 'Genre3', 
+            AppPrice: '$1.99', Playprice: '$2.99', img: '../components/icons/mario.jpg'},
+        {id: 6 ,title: 'Game6', developer: 'Developer3', Genre: 'Genre3', 
+            AppPrice: '$1.99', Playprice: '$2.99', img: '../components/icons/mario.jpg'}
+    ]
+
+    // export default{
+    //     data(){
+    //         return{
+    //             search: ""
+    //         }
+    //     }
+    // }
+</script>
+
 <template>
     <head>
         <link href='https://fonts.googleapis.com/css?family=Kanit' rel='stylesheet'>
@@ -27,105 +52,23 @@ import App from "@/App.vue";
                 <input type="text" placeholder="Search" id="search-input"/>
             </form>
         </div>
+        
         <div id="games-container">
-        <div class="game-card">
-            <div id="game-img-container">
-                    <img src="../components/icons/mario.jpg"/>
-                </div>
-                <hl>Title</hl>
-                <h3>developer</h3>
-                <h3>Genre</h3>
-                <p>App Store Price</p>
-                <p>Play Store Price</p>
-        </div>
-        <div class="game-card">
-            <div id="game-img-container">
-                    <img src="../components/icons/mario.jpg"/>
-                </div>
-                <hl>Title</hl>
-                <h3>developer</h3>
-                <h3>Genre</h3>
-                <p>App Store Price</p>
-                <p>Play Store Price</p>
-        </div>
-        <div class="game-card">
-            <div id="game-img-container">
-                    <img src="../components/icons/mario.jpg"/>
-                </div>
-                <hl>Title</hl>
-                <h3>developer</h3>
-                <h3>Genre</h3>
-                <p>App Store Price</p>
-                <p>Play Store Price</p>
-        </div>
-        <div class="game-card">
+            <div v-for="item in games" v-bind:key="item.id" class="game-card">
+                <a href="/game">
                 <div id="game-img-container">
                     <img src="../components/icons/mario.jpg"/>
                 </div>
-                <hl>Title</hl>
-                <h3>developer</h3>
-                <h3>Genre</h3>
-                <p>App Store Price</p>
-                <p>Play Store Price</p>
-        </div>
-        <div class="game-card">
-                <div id="game-img-container">
-                    <img src="../components/icons/mario.jpg"/>
-                </div>
-                <hl>Title</hl>
-                <h3>developer</h3>
-                <h3>Genre</h3>
-                <p>App Store Price</p>
-                <p>Play Store Price</p>
-        </div>
-        <div class="game-card">
-                <div id="game-img-container">
-                    <img src="../components/icons/mario.jpg"/>
-                </div>
-                <hl>Title</hl>
-                <h3>developer</h3>
-                <h3>Genre</h3>
-                <p>App Store Price</p>
-                <p>Play Store Price</p>
-        </div>
-        <div class="game-card">
-                <div id="game-img-container">
-                    <img src="../components/icons/mario.jpg"/>
-                </div>
-                <hl>Title</hl>
-                <h3>developer</h3>
-                <h3>Genre</h3>
-                <p>App Store Price</p>
-                <p>Play Store Price</p>
-        </div>
-        <div class="game-card">
-                <div id="game-img-container">
-                    <img src="../components/icons/mario.jpg"/>
-                </div>
-                <hl>Title</hl>
-                <h3>developer</h3>
-                <h3>Genre</h3>
-                <p>App Store Price</p>
-                <p>Play Store Price</p>
-        </div>
+                <hl class="game-title">{{item.title}}</hl>
+                <h3 class="developer-name">{{item.developer}}</h3>
+                <h3 class="game-genre">{{item.genre}}</h3>
+                <p>App Store Price: {{ item.AppPrice }}</p>
+                <p>Play Store Price: {{ item.Playprice }}</p>
+            </a>
+            </div>
         </div>
     </div>
     <footer class="footer-section">
     <bottomNavbar></bottomNavbar>
   </footer>
 </template>
-
-<script>
-    let games = [
-        {title: 'Game1', developer: 'Developer1', Genre: 'Genre1', 
-            AppPrice: '$0.99', Playprice: '$0.99', img: '../components/icons/mario.jpg'},
-        {title: 'Game2', developer: 'Developer2', Genre: 'Genre1', 
-            AppPrice: '$0.99', Playprice: '$0.99', img: '../components/icons/mario.jpg'},
-        {title: 'Game3', developer: 'Developer3', Genre: 'Genre1', 
-            AppPrice: '$0.99', Playprice: '$0.99', img: '../components/icons/mario.jpg'},
-        {title: 'Game4', developer: 'Developer4', Genre: 'Genre1', 
-            AppPrice: '$0.99', Playprice: '$0.99', img: '../components/icons/mario.jpg'},
-        {title: 'Game5', developer: 'Developer2', Genre: 'Genre3', 
-            AppPrice: '$1.99', Playprice: '$2.99', img: '../components/icons/mario.jpg'}
-    ]
-</script>
