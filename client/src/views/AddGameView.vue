fa896ea436ee02e6d537bf34382e51bc361fba82
Justin-Chua <justin.chua@ucalgary.ca>
2024-03-26 15:46:44 -0600

    add game page complete, desktop and mobile

--- client/src/views/AddGameView.vue
+++ client/src/views/AddGameView.vue
@@ -2,50 +2,79 @@
     import "../assets/footer.css";
     import "../assets/header.css";
     import "../assets/addGame.css";
+    import { onMounted, ref } from 'vue';
     import navbar from "../components/navbarView.vue";
     import bottomNavbar from "../components/bottomNavbarView.vue";
+
+    let loggedIn = ref(sessionStorage.getItem('loggedIn') === 'true');
+    let username = ref(sessionStorage.getItem('loggedInAs'));
+
+    const updateSessionData = () => {
+        //a session variable to check if logged in user is developer needed to access this page
+        //implement later
+        loggedIn.value = sessionStorage.getItem('loggedIn') === 'true';
+        username.value = sessionStorage.getItem('loggedInAs'); 
+        console.log(loggedIn.value);
+        console.log(username.value);
+    };
+
+    onMounted(() => {
+        updateSessionData();
+        document.getElementById('ag-image-upload').addEventListener('change', function(e) {
+            const file = e.target.files[0];
+            const reader = new FileReader();
+            reader.onloadend = function() {
+                document.getElementById('ag-preview-image').src = reader.result;
+            }
+            reader.readAsDataURL(file);
+        });
+    });
+
+    window.addEventListener('storage', updateSessionData);
+
+    defineExpose({loggedIn, username});
 </script>
 <template>
     <head>
         <link href='https://fonts.googleapis.com/css?family=Kanit' rel='stylesheet'>
         <link href="https://fonts.googleapis.com/css2?family=Major+Mono+Display&display=swap" rel="stylesheet">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css" rel="stylesheet">
     </head>
     <header class ="header-section">
         <navbar></navbar>
     </header>
     <body>
         <div class="ag-body-container">
-            <div class="ag-basic-grid">
-                <div class="ag-title-section">
-                    <form>
+            <form class="ag-form">
+                <div class="ag-basic-grid">
+                    <div class="ag-title-section">
                         <input id="ag-title" type="text" placeholder="enter title here...">
-                    </form>
-                </div>
-                <div class="ag-preview-section">
-                    <img id="ag-preview-image" src="../components/icons/mario.jpg">
-                    <div class="preview-text"><strong>PREVIEW</strong></div>
-                </div>
-                <div class="ag-button-section">
-                    <button id="ag-upload-button">Upload Media</button>
-                </div>
-                <div class="ag-description-section">
-                    <form>
+                    </div>
+                    <div class="ag-preview-section">
+                        <img id="ag-preview-image" src="../components/icons/mario.jpg"><br>
+                        <input type="file" id="ag-image-upload" name="ag-image-upload" accept="image/png, image/jpeg, image/svg">
+                    </div>
+                    <div class="ag-description-section">
                         <textarea id="ag-description" placeholder="Description"></textarea>
-                    </form>
+                    </div>
                 </div>
-            </div>
-            <div class="ag-advanced-flex">
-                <div>
-                    <form class="ag-advanced-form">
+                <div class="ag-advanced-flex">
+                    <div class="ag-advanced-inputs">
                         <input id="ag-link" type="text" placeholder="enter link here...">
-                        <input id="ag-price" type="text" placeholder="price"><br>
-                        <input id="ag-tags" type="text" placeholder="add tags...">
-                    </form>
+                        <input id="ag-google-play" type="text" placeholder="$0.00">
+                        <input id="ag-app-store" type="text" placeholder="$0.00">
+                        <select id="ag-genre">
+                            <option disable selected value>genre</option>
+                            <option value="adventure">adventure</option>
+                            <option value="horror">horror</option>
+                            <option value="rpg">rpg</option>
+                        </select>
+                    </div>
+                    <div>
+                        <input type="submit" id="ag-submit-button" value="SUBMIT">
+                    </div>
                 </div>
-                <div>
-                    <button id="ag-publish-button"><strong>PUBLISH</strong></button>
-                </div>
-            </div>
+            </form>
         </div>
     </body>
     <footer class="footer-section">
