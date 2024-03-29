891465dbc6be0924c7d1000a94a3fb9ae1e2b4a1
NathanRomphf <101217605+NathanRomphf@users.noreply.github.com>
2024-03-28 20:07:48 -0600

    Discussion and comments displayed from the database on the discussionpost page

--- server/controllers/discussion.js
+++ server/controllers/discussion.js
@@ -16,3 +16,31 @@ router.get('/getDiscussionsByGame', (req, res) => {
           } 
     });
 });
+
+router.get('/getDiscussionAndPosterById', (req, res) => {
+    const {id} = req.query;
+    databaseConnection.query('SELECT d.title, d.description, g.name, g.id, u.username FROM discussion as d, users as u, game as g WHERE d.poster_id = u.id AND d.game_id = g.id AND d.discussion_id = ?', 
+    [id], (err, result) => {
+        if (err) {
+            console.error('Error executing query:', err);
+            return res.status(500).send('Internal server error');
+          }
+          else{
+              res.json(result);
+          }     
+    });
+});
+
+router.get('/getDiscussionResponses', (req, res) => {
+    const {id} = req.query;
+    databaseConnection.query('SELECT c.comment_text, u.username, c.comment_ordinal FROM comments AS c, users AS u, discussion AS d WHERE c.discussion_id = d.discussion_id AND c.poster_id = u.id AND d.discussion_id = ? ORDER BY c.comment_ordinal ASC', 
+    [id], (err, result) => {
+        if (err) {
+            console.error('Error executing query:', err);
+            return res.status(500).send('Internal server error');
+          }
+          else{
+              res.json(result);
+          }    
+    });
+});
